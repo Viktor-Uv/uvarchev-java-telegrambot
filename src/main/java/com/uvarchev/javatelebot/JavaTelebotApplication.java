c30cbdf415dfa111d9f965631b5d1fe6cf165ce8
Viktor-Uv <sev-vic@yandex.com>
2023-11-25 03:13:58 +0100

    Initial commit

---
+++ src/main/java/com/uvarchev/javatelebot/JavaTelebotApplication.java
@@ -0,0 +1,13 @@
+package com.uvarchev.javatelebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTelebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvarchev/javatelebot/JavatelebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvarchev.javatelebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatelebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
