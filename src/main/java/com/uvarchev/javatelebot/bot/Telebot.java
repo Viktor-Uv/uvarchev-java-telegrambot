63f18e86f0aef0b431797d4fa8fb5ff89b641109
Viktor-Uv <sev-vic@yandex.com>
2023-12-04 12:04:36 +0100

    JAVATB-4: Added Command Processing, Enum for Start and Stop commands

--- src/main/java/com/uvarchev/javatelebot/bot/Telebot.java
+++ src/main/java/com/uvarchev/javatelebot/bot/Telebot.java
@@ -1,13 +1,16 @@
 package com.uvarchev.javatelebot.bot;
 
+import com.uvarchev.javatelebot.enums.BotCommands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class Telebot extends TelegramLongPollingBot {
 
-    final TelebotConfig config;
+    private final TelebotConfig config;
 
     public Telebot(TelebotConfig config) {
         this.config = config;
@@ -15,7 +18,73 @@ public class Telebot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Got update");
+        // Check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+
+            // Get chatId and user's name
+            Long chatId = update.getMessage().getChatId();
+            String firstName = update.getMessage().getChat().getFirstName();
+
+            // Check if the received message starts with a command
+            if (messageText.startsWith("/")) {
+                // Extract input command
+                String commandString = messageText.substring(1);
+
+                // Validate input command
+                if (isRecognisedCommand(commandString, chatId)) {
+                    // Convert commandString to enum constant
+                    BotCommands command = BotCommands.valueOf(commandString.toUpperCase());
+
+                    // Process command
+                    switch (command) {
+                        case START -> startCommandReceived(chatId, firstName);
+                        case STOP -> stopCommandReceived(chatId, firstName);
+                    }
+                } else {
+                    // Reply that command is not recognised
+                    unrecognisedCommandReceived(chatId);
+                }
+
+
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String firstName) {
+        String answer = "Hi, " + firstName + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void stopCommandReceived(long chatId, String firstName) {
+        String answer = "Bye, " + firstName + ", till next time!";
+        sendMessage(chatId, answer);
+    }
+
+    private void unrecognisedCommandReceived(long chatId) {
+        sendMessage(chatId, "Sorry, command was not recognised");
+    }
+
+    private boolean isRecognisedCommand(String commandString, long chatId) {
+        try {
+            BotCommands.valueOf(commandString.toUpperCase());
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Unrecognised command from chat ID " + chatId + ". " + e.getMessage());
+            return false;
+        }
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
---
+++ src/main/java/com/uvarchev/javatelebot/enums/BotCommands.java
@@ -0,0 +1,7 @@
+package com.uvarchev.javatelebot.enums;
+
+// Valid commands
+public enum BotCommands {
+    START,
+    STOP
+}
